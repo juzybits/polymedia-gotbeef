3459bc7eff519e58b83c3c7e190f1385d488e05c
juzybits <96890594+juzybits@users.noreply.github.com>
2022-09-26 03:32:39 +0000

    [web] 404 bucko

--- webapp/src/js/NotFound.tsx
+++ webapp/src/js/NotFound.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import bucko from '../img/bucko.png';
+
 export function NotFound(props) {
     return <React.Fragment>
         <h2>404</h2>
         <div>
-            In the 404 page
+            <img id='bucko' src={bucko} alt='Not found, bucko!' />
         </div>
     </React.Fragment>;
 }
