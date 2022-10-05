44483b666dd083bcb37445833202521f7e2d7532
juzybits <96890594+juzybits@users.noreply.github.com>
2022-10-05 13:00:08 +0000

    [web] add icons assets and create scripts/replace_icons.sh

---
+++ webapp/scripts/replace_icons.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Replace src/css/icons/ with an updated set of icons from fontello.com
+
+DOWNLOADS=$HOME/Downloads
+
+folder_name=${PWD##*/}
+if [[ "$folder_name" != 'webapp' ]]; then
+	me=`basename "$0"`
+	echo "[$me] Error: call this script only from the 'webapp' folder"
+	exit 1
+fi
+
+unzip $DOWNLOADS/fontello-*.zip
+rm $DOWNLOADS/fontello-*.zip
+rm fontello-*/demo.html
+rm fontello-*/README.txt
+rm -rf src/css/icons/
+mv fontello-*/ src/css/icons
