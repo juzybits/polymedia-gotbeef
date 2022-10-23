f39b8cf7cd3003d0a2e98f8b2802fad77ab30633
juzybits <96890594+juzybits@users.noreply.github.com>
2022-10-23 17:34:02 +0000

    [web] not found travolta

--- web/src/js/NotFound.tsx
+++ web/src/js/NotFound.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import bucko from '../img/bucko.png';
-
 export function NotFound(props: any) {
     return <React.Fragment>
-        <h2>404</h2>
+        <h2>Not found</h2>
         <div>
-            <img id='bucko' src={bucko} alt='Not found, bucko!' />
+            <img id='not-found-img' src='https://i.giphy.com/media/g01ZnwAUvutuK8GIQn/giphy.webp' alt='Not found' />
         </div>
     </React.Fragment>;
 }
