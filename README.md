3ef8be955718be978f72ef4f01323bd08842af70
juzybits <96890594+juzybits@users.noreply.github.com>
2022-12-28 23:41:16 -0100

    [web] improve homepage docs

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 A [Sui](https://sui.io/) package to create on-chain bets. It comes with a built-in escrow and voting functionality.
 
-- Anybody can create a new bet between 2 or more players (up to 256).
-- The winner is selected by a single judge, or by a quorum of judges (up to 32).
+- Anybody can create a new bet between 2 or more players.
+- The winner is selected by a single judge, or by a quorum of judges.
 - Funds can only be transferred to the winner, or refunded back to the players.
 
 Tired of [escrowing millions](https://twitter.com/GiganticRebirth/status/1503335929976664065) for your Twitter friends? Try _Got Beef?_.
--- web/src/js/Home.tsx
+++ web/src/js/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function Home(props: any) {
     useEffect(() => {
@@ -23,29 +24,29 @@ export function Home(props: any) {
             <p>
                 <i>Got Beef?</i> is a dApp to create on-chain bets on the <a href='https://sui.io/' className='rainbow' target='_blank'>Sui network</a>.
             </p>
+            <p>
+                Bet objects serve as escrows (hold player funds) and provide a voting mechanism.
+            </p>
 
             <span className='section-title' onClick={() => toggleSection('section-how-to-play')}>{showHowToPlay?'-':'+'} <span>How to play:</span></span>
             {showHowToPlay &&
             <div className='section-body' id='section-how-to-play'>
-                <ul>
-                    <li>Anybody can create a new bet between two or more players (up to 256).</li>
-                    <li>A new bet starts in the "funding" phase, which means players can fund the bet.</li>
-                    <li>Once all players have sent their funds, the bet moves to the "voting" phase.</li>
-                    <li>The winner is then selected by either one judge or by a quorum of judges (up to 32).</li>
-                    <li>The code ensures that player funds can only be transferred to the winner, or refunded back to the players if the bet is cancelled.</li>
-                </ul>
+                <ol>
+                    <li>Create a <Link to='/new'>new bet</Link> between two or more players (bet starts in the "funding" phase).</li>
+                    <li>Players send and lock their funds in the bet object (bet moves to the "voting" phase).</li>
+                    <li>The winner is selected by the judge / quorum of judges, and the funds get transferred to the winner (bet moves to the "settled" phase").</li>
+                </ol>
             </div>}
 
             <span className='section-title' onClick={() => toggleSection('section-what-is-for')}>{showWhatIsFor?'-':'+'} <span>What is this for?</span></span>
             {showWhatIsFor && <div className='section-body' id='section-what-is-for'>
-                In a scenario&nbsp;
-                <a href='https://twitter.com/GiganticRebirth/status/1503335929976664065' className='rainbow' target='_blank'>such as this one</a>, a <i>Got Beef?</i> bet could have been used:
+                In <a href='https://twitter.com/GiganticRebirth/status/1503335929976664065' className='rainbow' target='_blank'>this scenario</a>, <i>Got Beef?</i> could have been used:
                 <ul>
                     <li>player 1 = GCR</li>
                     <li>player 2 = Do Kwon</li>
                     <li>judge&nbsp;&nbsp;&nbsp;&nbsp;= Cobie</li>
                 </ul>
-                <i>Got Beef?</i> bet objects behave as escrows (they hold player funds) and they also provide a voting mechanism.
+                <i>Got Beef</i> bets ensures that funds can only be sent to the winner's address (one of the players). This is a lot safer than trusting a human escrow to hold the funds in their own wallet.
             </div>}
 
             <span className='section-title' onClick={() => toggleSection('section-where-addr')}>{showWhereAddr?'-':'+'} <span>Where do the addresses come from?</span></span>
